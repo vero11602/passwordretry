235f9ecfa3a42395541588d57b83f95540608425
Ingrid Jiang <vero11602@gmail.com>
2020-06-29 15:55:32 +0800

    while i>0

--- passwordretry.py
+++ passwordretry.py
@@ -1,13 +1,22 @@
 password = 'a123456'
 i = 3
-while True :
-	pwd = input ('請輸入密碼')
+while  i > 0 :
+	pwd = input ('密碼多少')
 	if pwd == password :
-		print ('登入成功!!')
+		print ('密碼正確 ，登入成功')
 		break
-	else :
-		i = i-1
-		print ('輸入錯誤!!還有' , i ,'次機會')
-		if i == 0 :
-			break
+	else : 
+		i = i - 1
+		print ('登入失敗，還有' , i , '機會')
+		
+		
+		
+
+
+
+
+
+
+
+
 
